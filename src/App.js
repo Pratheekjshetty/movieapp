120eb2a2877a43165e37f4f13d0c97ada5858897
Pratheekjshetty <pratheekjshetty6696@gmail.com>
2024-09-15 13:49:51 +0530

    dependencies installed

--- src/App.js
+++ src/App.js
@@ -2,7 +2,10 @@ import React from "react";
 
 function App() {
   return (
-    <div className="App">
+    <div className="h-header bg-main w-full">
+      <h1 className="text-3xl text-star font-bold underline">
+        Hello world!
+      </h1>
     </div>
   );
 }
