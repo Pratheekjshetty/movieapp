239a27dd10dc59ed8cc90b3869c96b1d6f7291b4
Pratheekjshetty <pratheekjshetty6696@gmail.com>
2024-09-20 21:26:28 +0530

    MovieRates page updated

--- client/src/Screens/SingleMovie/SingleMovie.jsx
+++ client/src/Screens/SingleMovie/SingleMovie.jsx
@@ -4,7 +4,7 @@ import { useParams } from 'react-router-dom'
 import { Movies } from '../../Data/MovieData/MovieData'
 import MovieInfo from '../../Components/Single/MovieInfo/MovieInfo'
 import MovieCasts from '../../Components/Single/MovieCasts/MovieCasts'
-
+import  MovieRates from '../../Components/Single/MovieRates/MovieRates'
 const SingleMovie = () => {
   const {id} = useParams()
   const movie = Movies.find((movie) => movie.name === id)
@@ -13,6 +13,7 @@ const SingleMovie = () => {
       <MovieInfo movie={movie}/>
       <div className='container mx-auto min-h-screen px-2 my-6'>
         <MovieCasts selectedMovie={movie.name}/>
+        <MovieRates movie={movie}/>
       </div>
     </Layout>
   )
